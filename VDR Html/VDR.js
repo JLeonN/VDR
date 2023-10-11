b1d5f93ef9c6dd20ae28ae1b20e360f21a5f8a49
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2023-10-10 23:02:28 -0300

    Dia - 1

--- VDR Html/VDR.js
+++ VDR Html/VDR.js
@@ -1,7 +1,21 @@
-document.getElementById('cargarContenido').addEventListener('click', function () {
-  fetch('ramcopia.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('contenedor').innerHTML = data;
-    });
+// document.getElementById('cargarContenido').addEventListener('click', function () {
+//   fetch('ramcopia.html')
+//     .then(response => response.text())
+//     .then(data => {
+//       document.getElementById('contenedor').innerHTML = data;
+//     });
+// });
+
+
+
+
+document.getElementById("toggle-button").addEventListener("click", function () {
+  var menu = document.getElementById("menu");
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    this.textContent = "Ocultar Menú";
+  } else {
+    menu.classList.add("hidden");
+    this.textContent = "Mostrar Menú";
+  }
 });
