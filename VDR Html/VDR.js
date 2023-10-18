b65f8bf3bd4548edb29f4a3e9e1613dd9a696518
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2023-10-17 22:09:38 -0300

    Estetica

--- VDR Html/VDR.js
+++ VDR Html/VDR.js
@@ -9,13 +9,28 @@
 
 
 
-document.getElementById("toggle-button").addEventListener("click", function () {
-  var menu = document.getElementById("menu");
-  if (menu.classList.contains("hidden")) {
-    menu.classList.remove("hidden");
-    this.textContent = "Ocultar Menú";
-  } else {
-    menu.classList.add("hidden");
-    this.textContent = "Mostrar Menú";
-  }
-});
+// document.getElementById("toggle-button").addEventListener("click", function () {
+//   var menu = document.getElementById("menu");
+//   if (menu.classList.contains("hidden")) {
+//     menu.classList.remove("hidden");
+//     this.textContent = "Ocultar Menú";
+//   } else {
+//     menu.classList.add("hidden");
+//     this.textContent = "Mostrar Menú";
+//   }
+// });
+
+
+
+
+// Obtén una referencia al elemento con id "contenido"
+var contenidoDiv = document.getElementById("contenido");
+
+// Crea un nuevo elemento div para el contenido HTML que deseas insertar
+var nuevoContenido = document.createElement("div");
+
+// Agrega el código HTML que deseas insertar
+nuevoContenido.innerHTML = "<p>lol</p>";
+
+// Agrega el nuevo contenido al div con id "contenido"
+contenidoDiv.appendChild(nuevoContenido);
